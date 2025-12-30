2e2bf177c2411114adf60ab4308bc666a0a1526b
Edoardo <154237095+EdoUnimib@users.noreply.github.com>
2025-12-30 20:44:59 +0100

    Add files via upload

---
+++ ProgettoESAME/mainwindow.cpp
@@ -0,0 +1,223 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    connect(ui->calendarWidget, &QCalendarWidget::clicked,
+            this, &MainWindow::onDateClicked);
+    connect(ui->checkEndTime, &QCheckBox::toggled,
+            ui->timeEnd, &QTimeEdit::setEnabled);
+    connect(ui->btnSaveTask, &QPushButton::clicked,
+            this, &MainWindow::onSaveTaskClicked);
+    connect(ui->btnUpdateTask, &QPushButton::clicked,
+            this, &MainWindow::onUpdateTaskClicked);
+    connect(ui->btnDeleteTask, &QPushButton::clicked,
+            this, &MainWindow::onDeleteTaskClicked);
+    connect(ui->tableActivities, &QTableWidget::itemSelectionChanged,
+            this, &MainWindow::onTableSelectionChanged);
+    connect(ui->tableActivities, &QTableWidget::itemClicked,
+            this, &MainWindow::onTableItemClicked);
+
+
+    selectedDate = QDate::currentDate();
+    ui->calendarWidget->setSelectedDate(selectedDate);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::onDateClicked(const QDate &date){
+    selectedDate = date;
+    refreshTable(selectedDate);
+}
+
+void MainWindow::refreshTable(const QDate &date){
+
+    ui->tableActivities->setRowCount(0);
+    QList<Task> tasksOfDay = tasksByDate.value(date);
+
+    for(int i = 0; i < tasksOfDay.size(); ++i){
+        int row = ui->tableActivities->rowCount();
+        ui->tableActivities->insertRow(row);
+
+        QString typeText;
+        if(tasksOfDay[i].type == TaskType::Event){
+            typeText = "Evento";
+        }else{
+            typeText = "Attività";
+        }
+        ui->tableActivities->setItem(row, 0, new QTableWidgetItem(typeText));
+        ui->tableActivities->setItem(row, 1, new QTableWidgetItem(tasksOfDay[i].title));
+        ui->tableActivities->setItem(row, 2, new QTableWidgetItem(tasksOfDay[i].startTime.toString("HH:mm")));
+        if(tasksOfDay[i].hasEndTime){
+             ui->tableActivities->setItem(row, 3, new QTableWidgetItem(tasksOfDay[i].endTime.toString("HH:mm")));
+        } else {
+             ui->tableActivities->setItem(row, 3, new QTableWidgetItem("-"));
+        }
+        ui->tableActivities->setItem(row, 4, new QTableWidgetItem(tasksOfDay[i].frequency));
+
+    }
+
+}
+
+void MainWindow::onSaveTaskClicked(){
+
+    QTime oraInizio = ui->timeStart->time();
+    QTime oraFine;
+
+    if(ui->checkEndTime->isChecked()){
+        oraFine = ui->timeEnd->time();
+    }else{
+        oraFine = oraInizio;
+    }
+
+    if(esisteSovrapposizione(selectedDate, oraInizio, oraFine)){
+        QMessageBox::warning(this, "Sovrapposizione evento", "Esiste già un'attività in questa fascia oraria.");
+        return;
+    }
+
+
+    Task task;
+
+    if(ui->comboType->currentText()=="Evento"){
+        task.type = TaskType::Event;
+    }else{
+        task.type = TaskType::Activity;
+    }
+
+    task.title = ui->editTitle->text();
+    task.startTime = ui->timeStart->time();
+
+    task.hasEndTime = ui->checkEndTime->isChecked();
+    if(task.hasEndTime){
+        task.endTime = ui->timeEnd->time();
+    }
+
+    task.frequency = ui->comboFrequency->currentText();
+    task.completed = false;
+
+    tasksByDate[selectedDate].append(task);
+    refreshTable(selectedDate);
+
+}
+
+void MainWindow::onUpdateTaskClicked(){
+
+    if(index_task_da_editare < 0)
+        return;
+
+    QList<Task> &tasksOfDay = tasksByDate[selectedDate];
+
+    if(index_task_da_editare >= tasksOfDay.size())
+        return;
+
+    Task &task = tasksOfDay[index_task_da_editare];
+
+    if(ui->comboType->currentText()=="Evento"){
+        task.type = TaskType::Event;
+    }else{
+        task.type = TaskType::Activity;
+    }
+
+    task.title = ui->editTitle->text();
+    task.startTime = ui->timeStart->time();
+
+    task.hasEndTime = ui->checkEndTime->isChecked();
+    if(task.hasEndTime){
+        task.endTime = ui->timeEnd->time();
+    }
+
+    task.frequency = ui->comboFrequency->currentText();
+
+    index_task_da_editare = -1;
+
+    refreshTable(selectedDate);
+
+}
+
+void MainWindow::onTableSelectionChanged(){
+
+    bool selection = !ui->tableActivities->selectedItems().isEmpty();
+    ui->btnDeleteTask->setEnabled(selection);
+    ui->btnUpdateTask->setEnabled(selection);
+}
+
+void MainWindow::onDeleteTaskClicked(){
+
+    int row = ui->tableActivities->currentRow();
+
+    // Nessuna riga selezionata
+    if(row < 0)
+        return;
+
+    // recupero la lista di attività del giorno
+    QList<Task> &tasksOfDay = tasksByDate[selectedDate];
+
+    // controllo di sicurezza
+    if(row >= tasksOfDay.size())
+        return;
+
+    // rimuovo l'attività
+    tasksOfDay.removeAt(row);
+
+    refreshTable(selectedDate);
+}
+
+void MainWindow::onTableItemClicked(QTableWidgetItem *item){
+
+    int row = item->row();
+    QList<Task> &tasksOfDay = tasksByDate[selectedDate];
+
+    if(row < 0 || row >= tasksOfDay.size())
+        return;
+
+    const Task &task = tasksOfDay[row];
+    index_task_da_editare = row;
+
+    ui->comboType->setCurrentText(task.type == TaskType::Event ? "Evento" : "Attività");
+    ui->editTitle->setText(task.title);
+    ui->timeStart->setTime(task.startTime);
+    ui->checkEndTime->setChecked(task.hasEndTime);
+    ui->timeEnd->setEnabled(task.hasEndTime);
+    if(task.hasEndTime)
+        ui->timeEnd->setTime(task.endTime);
+
+    ui->comboFrequency->setCurrentText(task.frequency);
+}
+
+bool MainWindow::esisteSovrapposizione(const QDate &date, const QTime &start, const QTime &end){
+
+    const QList<Task> &tasksOfDay = tasksByDate.value(date);
+
+    for(int i = 0; i < tasksOfDay.size(); ++i){
+
+        QTime otherStart = tasksOfDay[i].startTime;
+        QTime otherEnd;
+
+        if(tasksOfDay[i].hasEndTime){
+            otherEnd = tasksOfDay[i].endTime;
+        }else{
+            otherEnd = tasksOfDay[i].startTime;
+        }
+
+        if((start < otherEnd) && (end > otherStart)){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+
+
+
+
---
+++ ProgettoESAME/mainwindow.h
@@ -0,0 +1,63 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDate>
+#include <QMap>
+#include <QTableWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+enum class TaskType{
+    Event,
+    Activity
+};
+
+struct Task{
+   TaskType type;
+   QString title;
+   QTime startTime;
+   QTime endTime;
+   bool hasEndTime;
+   QString frequency;
+   bool completed;
+};
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+    QDate selectedDate;
+    QMap<QDate, QList<Task>> tasksByDate;
+
+    int index_task_da_editare = -1;
+
+private slots:
+    void onDateClicked(const QDate &date);
+    void onSaveTaskClicked();
+    void onUpdateTaskClicked();
+    void onDeleteTaskClicked();
+    void onTableSelectionChanged();
+    void onTableItemClicked(QTableWidgetItem *item);
+
+private:
+    void refreshTable(const QDate &date);
+    bool esisteSovrapposizione(const QDate &date, const QTime &start, const QTime &end);
+
+
+
+
+
+
+
+};
+#endif // MAINWINDOW_H
